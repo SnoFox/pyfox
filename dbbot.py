fa9b4d5167077e1a82b35cb546bf4278a0ea12d5
David Dixon II <scrapii407@gmail.com>
2012-03-07 22:01:35 -0500

    Removed -bg. Use screen in terminal.

--- dbbot.py
+++ dbbot.py
@@ -35,64 +35,6 @@ def main():
 
 		sparks.newClient(server)
 
-	# Fork Background Stolen from Skyakdar! Thank you dKingston
-	# PROTIP: This might not work.... 
-	if '-bg' in sys.argv:
-		try:
-			pid = os.fork()
-		except OSError as e:
-			print e.errno, e.strerror
-	
-		# This is the child process.
-		if pid == 0:
-			os.setsid()
-	
-			# Now the child fork()'s a child in order to prevent
-			# acquisition of a controlling terminal.
-			try:
-				pid = os.fork()
-			except OSError as e:
-				print e.errno, e.strerror
-	
-			# This is the second child process.
-			if pid == 0:
-				os.chdir(os.getcwd())
-				os.umask(0)
-
-			# This is the first child.
-			else:
-				print 'Running dbbot in background mode [%s] (%s)' % (pid, os.getcwd())
-				os._exit(0)
-		else:
-			os._exit(0)
-	
-		# Try to write the pid file.
-		try:
-			pid_file = open('dbbot.pid', 'w')
-			pid_file.write(str(os.getpid()))
-			pid_file.close()
-		except IOError as e:
-			print >> sys.stderr, 'Unable to write PID to %s: %s.' % (os.getpid(), os.strerror(e.args[0]))
-	
-		# Try to close all open file descriptors.
-		# If we can't find the max number, just close
-		# the first 256.
-		try:	    
-			maxfd = os.sysconf('SC_OPEN_MAX')
-		except (AttributeError, ValueError):
-			maxfd = 256
-		
-		for fd in range(0, maxfd):
-			try:		
-				os.close(fd)    
-			except OSError:
-				pass
-		    
-		# Redirect the standard file descriptors to /dev/null.
-		os.open('/dev/null', os.O_RDONLY)		     
-		os.open('/dev/null', os.O_RDWR)  
-		os.open('/dev/null', os.O_RDWR)
-
 	sparks.loop()
 
 if __name__ == '__main__':
