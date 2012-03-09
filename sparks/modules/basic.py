0748c344560d621014e4e72595996169ec7d7abf
David Dixon II <scrapii407@gmail.com>
2012-03-09 12:45:44 -0500

    Add identification to basic

--- sparks/modules/basic.py
+++ sparks/modules/basic.py
@@ -1,6 +1,19 @@
 # This file is basic for dbbot
 
 def sr_001(irc, params):
+	# Nick Authentication
+	auth = irc.config['modules']['auth']
+
+	if 'nick' in auth: # Authenticate using a nick
+		if type(auth['nick']) == bool:
+			irc.privmsg(auth['service'], "%s %s %s" % (auth['command'], self.config['nick'], auth['password']))
+		else:
+			irc.privmsg(auth['service'], "%s %s %s" % (auth['command'], auth['nick'], auth['password']))
+
+	else:
+		irc.privmsg(auth['service'], "%s %s" % (auth['command'], auth['password']))
+
+	# Join channels
 	channels = irc.config['modules']['join']
 
 	irc.join(channels)
