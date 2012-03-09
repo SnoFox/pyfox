4b94a1d9df157eca5c3181ca24fa6048fb2f7b0a
David Dixon II <scrapii407@gmail.com>
2012-03-09 14:30:05 -0500

    Catch PREFIXES and create channel lists

--- sparks/modules/basic.py
+++ sparks/modules/basic.py
@@ -1,4 +1,5 @@
 # This file is basic for dbbot
+from re import search
 
 def sr_001(irc, params):
 	# Nick Authentication
@@ -17,3 +18,38 @@ def sr_001(irc, params):
 	channels = irc.config['modules']['join']
 
 	irc.join(channels)
+
+def sr_005(irc, params):
+	# Find all the modes!
+
+        line = ' '.join(params) if 'PREFIX' in ' '.join(params) else None
+
+        if line:
+
+                rprefix = search('PREFIX=\(([^)]+)\)([^ ]+)', line).groups()
+                irc.statusmodes = dict(zip(rprefix[1], rprefix[0]))
+
+def sr_353(irc, params):
+	# Create a nicklist for each channel
+        if not hasattr(irc, 'chanlists'):
+		irc.chanlists = {}
+
+        # Coded by _habnabit #python Freenode. Greatly appreciated!
+        ret = {}
+        ret2 = {}
+
+        for nick in params[3:]:
+                nick = nick.lstrip(':')
+                nick_modes = []
+
+                for e, char in enumerate(nick):
+                        if char not in irc.statusmodes:
+                                ret[nick[e:]] = nick_modes
+                                break
+
+                        nick_modes.append(irc.statusmodes[char])
+
+        for r in ret:
+                ret2[r] = ''.join(ret[r])
+
+        irc.chanlists[params[2]] = ret2
