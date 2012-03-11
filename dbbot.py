9fb6351581447a380027cec51b5bfe2d1f89432a
David Dixon II <scrapii407@gmail.com>
2012-03-10 19:17:28 -0500

    Allow connection to a specific network

--- dbbot.py
+++ dbbot.py
@@ -15,23 +15,43 @@ def main():
 
 	# Basic Setup
 	signal.signal(signal.SIGINT, on_sigint)
-	
-	for name, conf in config.items():
 
-		conf['name'] = name
-		conf['admins'] = [] if 'admins' not in conf else conf['admins'].split(',')
+	if len(sys.argv) == 2: # Connect to a specific network
+		for name, conf in config.items():
+			if name == sys.argv[1]:
 
-		if not conf['triggers']:
-			print('Network triggers have not been setup for %s. Stopping!' % conf['name'])
-			exit()
+				conf['name'] = name
+				conf['admins'] = [] if 'admins' not in conf else conf['admins'].split(',')
 
-		conf['triggers'] = conf['triggers'].split(',', 1)
+				if not conf['triggers']:
+					print('Network triggers have not been setup for %s. Stopping!' % conf['name'])
+					exit()
 
-		if len(conf['triggers']) != 2:
-			print('Triggers have not been setup correctly for %s. Stopping!' % conf['name'])
-			exit()
+				conf['triggers'] = conf['triggers'].split(',', 1)
 
-		sparks.newClient(conf)
+				if len(conf['triggers']) != 2:
+					print('Triggers have not been setup correctly for %s. Stopping!' % conf['name'])
+					exit()
+
+				sparks.newClient(conf)
+
+	else:
+		for name, conf in config.items():
+
+			conf['name'] = name
+			conf['admins'] = [] if 'admins' not in conf else conf['admins'].split(',')
+
+			if not conf['triggers']:
+				print('Network triggers have not been setup for %s. Stopping!' % conf['name'])
+				exit()
+
+			conf['triggers'] = conf['triggers'].split(',', 1)
+
+			if len(conf['triggers']) != 2:
+				print('Triggers have not been setup correctly for %s. Stopping!' % conf['name'])
+				exit()
+
+			sparks.newClient(conf)
 
 	sparks.loop()
 
