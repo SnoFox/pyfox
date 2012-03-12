763a8d1b876313ff3ead168d77b6792d7536d836
Josh Johnson <SnoFox@SnoFox.net>
2012-03-12 00:26:00 -0700

    Woo! Mode parser and event emitter

--- sparks/modules/basic.py
+++ sparks/modules/basic.py
@@ -1,6 +1,7 @@
 # This file is basic for dbbot
 from re import search # needs regex for dbdii's prefix "sorter"
 from time import sleep
+from sparks.modules import dbmods # required to emit events from the mode parser
 
 def tsr_001(irc, client, params):
 	# Nick Authentication
@@ -38,6 +39,7 @@ def sr_005(irc, client, params):
 	while x < 5:
 		# literally "hack" off ":are supported by this server"
 		# Will break on broken, non-RFC-following servers
+		print "pop %d" % x
 		params.pop()
 		x += 1
 
@@ -52,9 +54,14 @@ def sr_005(irc, client, params):
 
 		# ok; now utilize some awesome ones for the core
 		if feature[0] == "PREFIX":
+			# PREFIX=(aohv)!@%+
 			# Dave's original code, modified to work here
 			rprefix = search('\(([^)]+)\)([^ ]+)', value).groups()
 			irc.statusmodes = dict(zip(rprefix[1], rprefix[0]))
+			irc.typeemodes = rprefix[0] # stick the letters in a list 
+										# because I can't figure out how
+										# to iterate through a dict's values
+										# efficiently - SnoFox
 
 		if feature[0] == "CHANMODES":
 			# CHANMODES=eIbq,k,flj,CEFGJKLMOPQSTcdgimnprstz
@@ -64,7 +71,7 @@ def sr_005(irc, client, params):
 			# A: listmodes
 			# B: parameter when set/unset
 			# C: parameter only when set
-			# D: no param
+			# D: no param (flagmodes)
 			# E: prefix mode (not dealt with here)
 
 			# indecies of the list:
@@ -118,25 +125,74 @@ def sr_353(irc, client, params):
 	irc.chanlists[params[2]] = ret2
 
 def sr_part(irc, client, params):
-	if params[0].startswith(':'):
-		params[0] = params[0][1:]
-
 	irc.push("NAMES %s" % params[0])
 
 def sr_join(irc, client, params):
-	if params[0].startswith(':'):
-		params[0] = params[0][1:]
-
 	irc.push("NAMES %s" % params[0])
 
 def sr_mode(irc, client, params):
-
-	print client
-
-	if params[0].startswith(':'):
-		params[0] = params[0][1:]
-
 	irc.push("NAMES %s" % params[0])
+
+	target = params[0]
+	# Mode parser
+	# Sends out mode events for other modules to use
+	# - SnoFox
+
+	#print "sr_mode(): params = %s" % params
+	#sr_mode(): params = ['#foxtest', '-kCT', '*']
+	
+	# First of all, channel mode or user mode?
+
+	if target[0] in irc.isupport['CHANTYPES']:
+		chmode = True 
+	else:
+		chmode = False
+	
+	adding = True # true = adding a mode; false = removing
+	paramNum = 2  # count of which param we're using 
+				  # This starts at 2 due to Sparks' garbage in the params list :\
+
+	# XXX: This will crash the bot if the IRCd lied to us in the 005 version string
+	# then later sends us a mode line that doesn't agree with our knowledge of modes
+	# That ... Should probably be fixed.
+	for char in params[1]:
+		# Are we adding or subtracting the mode?
+		if char == '+':
+			adding = True
+		elif char == '-':
+			adding = False
+		else:
+			param = None
+			if chmode:
+				# Check to see what kind of mode this is
+				if char in irc.typeemodes or char in irc.listmodes or char in irc.typebmodes or (char in irc.typecmodes and adding):
+					# These types of modes have params
+					# Launch events! Woo!
+					for n, mod in enumerate(dbmods):
+						# Dunno what the tsr_ events are for; let's not emit them to save another .2ms
+						# (for now)
+						#if hasattr(mod, 'tsr_%s' % command.lower() ):
+						#	cmd = getattr(mod, 'tsr_%s' % command.lower())
+						#	threader(cmd, (self, client, params))
+						if hasattr( mod, 'sr_chmode_%s' % char ):
+							cmd = getattr( mod, 'sr_chmode_%s' % char )
+							cmd( irc, client, target, adding, params[paramNum] )
+					paramNum += 1
+					continue
+				else:
+					# This is a letter, hopefully, or at least a mode.
+					# Fire off an event for it
+					for n, mod in enumerate(dbmods):
+						if hasattr( mod, 'sr_chmode_%s' % char ):
+							cmd = getattr( mod, 'sr_chmode_%s' % char )
+							cmd( irc, client, target, adding, None )
+			else:
+				# Usermode get
+				print "Got user mode %s. Not a fuck was given that modeline." % char
+
+
+
+
 
 def ca_chanlist(irc, client, channel, params):
 	irc.privmsg(channel, irc.chanlists[channel])
